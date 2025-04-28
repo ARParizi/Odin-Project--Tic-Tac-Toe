d9b1eb94443931d291cbfc52d8f62169a71ad2ef
ARParizi <52738836+ARParizi@users.noreply.github.com>
2025-04-28 18:24:52 +0330

    Change UI board addresses

--- javascript.js
+++ javascript.js
@@ -26,12 +26,12 @@ renderConsole();
 game();
 function game() {
     while(true) {
-        const ans = parseInt(prompt(`Player ${currentPlayer}, Which position would you like? (0 - 8)`));
+        const ans = parseInt(prompt(`Player ${currentPlayer}, Which position would you like? (1 - 9)`));
 
-        if (isNaN(ans) || ans < 0 || ans > 8)
+        if (isNaN(ans) || ans < 1 || ans > 9)
             continue;
 
-        const {x, y} = translate1DAddrTo2D(ans);
+        const {x, y} = translate1DAddrTo2D(ans - 1);
         if(gameState[x][y] !== '-')
             continue;
 
@@ -63,8 +63,8 @@ function isBoardFull() {
 }
 
 function renderConsole() {
-    console.log(`${gameState[0][0]} ${gameState[0][1]} ${gameState[0][2]}   0 1 2`);
-    console.log(`${gameState[1][0]} ${gameState[1][1]} ${gameState[1][2]}   3 4 5`);
-    console.log(`${gameState[2][0]} ${gameState[2][1]} ${gameState[2][2]}   6 7 8`);
+    console.log(`${gameState[0][0]} ${gameState[0][1]} ${gameState[0][2]}   1 2 3`);
+    console.log(`${gameState[1][0]} ${gameState[1][1]} ${gameState[1][2]}   4 5 6`);
+    console.log(`${gameState[2][0]} ${gameState[2][1]} ${gameState[2][2]}   7 8 9`);
     console.log();
 }
